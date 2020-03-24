8c1576c5adef9dd9166e07dae1a38dd73f53bd35
Pavel Petrukhin <petrukhp@tcd.ie>
2020-03-24 23:51:59 +0300

    Final version of the assignment

--- WordLinks.java
+++ WordLinks.java
@@ -1,56 +1,62 @@
 /* SELF ASSESSMENT 
 
 1. readDictionary
-- I have the correct method definition [Mark out of 5:]
-- Comment: 
-- My method reads the words from the "words.txt" file. [Mark out of 5:]
-- Comment: 
-- It returns the contents from "words.txt" in a String array or an ArrayList. [Mark out of 5:]
-- Comment: 
+- I have the correct method definition [Mark out of 5:5]
+- Comment:	The method returns an ArrayList of Strings and takes no parameters 
+- My method reads the words from the "words.txt" file. [Mark out of 5:5]
+- Comment:	The method opens file "words.txt" and reads all the words from it correctly 
+- It returns the contents from "words.txt" in a String array or an ArrayList. [Mark out of 5:5]
+- Comment:	Yes, the method returns an ArrayList of Strings, which has all the words from the file	
 
 2. readWordList
-- I have the correct method definition [Mark out of 5:]
-- Comment: 
-- My method reads the words provided (which are separated by commas, saves them to an array or ArrayList of String references and returns it. [Mark out of 5:]
-- Comment: 
+- I have the correct method definition [Mark out of 5:5]
+- Comment: The method is defined correctly, it takes no parameters and returns an ArrayList of strings, which are entered
+by the user
+- My method reads the words provided (which are separated by commas, saves them to an array or ArrayList of String references and returns it. [Mark out of 5:5]
+- Comment: It uses a split method with a special regular expression, so as to separate words and 
+return them as an ArrayList of Strings
 
 3. isUniqueList
-- I have the correct method definition [Mark out of 5:]
-- Comment: 
-- My method compares each word in the array with the rest of the words in the list. [Mark out of 5:]
-- Comment: 
-- Exits the loop when a non-unique word is found. [Mark out of 5:]
-- Comment: 
-- Returns true is all the words are unique and false otherwise. [Mark out of 5:]
-- Comment: 
+- I have the correct method definition [Mark out of 5:5]
+- Comment:	The method takes an ArrayList of Strings and returns a boolean
+- My method compares each word in the array with the rest of the words in the list. [Mark out of 5:5]
+- Comment:	Yes, it does. It also does that efficiently, by comparing each element only with those 
+elements that have a larger index
+- Exits the loop when a non-unique word is found. [Mark out of 5:5]
+- Comment:	It returns false if a non-unique word is found. 
+- Returns true is all the words are unique and false otherwise. [Mark out of 5:5]
+- Comment:	If just one value is not unique the method immediately returns false, otherwise if all 
+values are unique it returns true
 
 4. isEnglishWord
-- I have the correct method definition [Mark out of 5:]
-- Comment: 
-- My method uses the binarySearch method in Arrays library class. [Mark out of 3:]
-- Comment: 
-- Returns true if the binarySearch method return a value >= 0, otherwise false is returned. [Mark out of 2:]
-- Comment: 
+- I have the correct method definition [Mark out of 5:5]
+- Comment: It takes a String, and determines whether it is an English word (returns a boolean)
+- My method uses the binarySearch method in Arrays library class. [Mark out of 3:3]
+- Comment: Yes, it does. The binarySearch method returns an integer, which is stored in a special variable.
+- Returns true if the binarySearch method return a value >= 0, otherwise false is returned. [Mark out of 2:2]
+- Comment: Yes, it does exactly that.
 
 5. isDifferentByOne
-- I have the correct method definition [Mark out of 5:]
-- Comment: 
-- My method loops through the length of a words comparing characters at the same position in both words searching for one difference. [Mark out of 10:]
-- Comment: 
+- I have the correct method definition [Mark out of 5:5]
+- Comment: Yes, it takes two Strings and returns a boolean, identifying whether they are different by one character
+- My method loops through the length of a words comparing characters at the same position in both words searching for one difference. [Mark out of 10:10]
+- Comment: Yes, it does exactly that.
 
 6. isWordChain
-- I have the correct method definition [Mark out of 5:]
-- Comment: 
-- My method calls isUniqueList, isEnglishWord and isDifferentByOne methods and prints the appropriate message [Mark out of 10:]
-- Comment: 
+- I have the correct method definition [Mark out of 5:5]
+- Comment:	Yes, it takes an ArrayList of Strings and determines whether it is a proper word chain by returning a boolean
+- My method calls isUniqueList, isEnglishWord and isDifferentByOne methods and prints the appropriate message [Mark out of 10:10]
+- Comment: It uses all of the methods listed above, but returns a boolean instead of just printing out the result. 
+I think it is more generic. 
 
 7. main
-- Reads all the words from file words.txt into an array or an ArrayList using the any of teh Java.IO classes covered in lectures [Mark out of 10:]
-- Comment: 
-- Asks the user for input and calls isWordChain [Mark out of 5:]
-- Comment: 
+- Reads all the words from file words.txt into an array or an ArrayList using the any of the Java.IO classes covered in lectures [Mark out of 10:10]
+- Comment: Yes, it does. And that ArrayList is static so that I do not need to run the code for reading words every time 
+I need to check if a given String is an English word.
+- Asks the user for input and calls isWordChain [Mark out of 5:5]
+- Comment: Program does exactly what specified above and the input and output are the same as in the sample.
 
- Total Mark out of 100 (Add all the previous marks):
+ Total Mark out of 100 (Add all the previous marks): 100
 */
 
 import java.io.InputStreamReader;
@@ -91,11 +97,23 @@ public class WordLinks
 		
 		String words = input.readLine().toLowerCase();
 		
-		String[] splittedWords = words.split(",[^a-z]?");
+		String[] splittedWords = words.split(", *");
 			
 		if(splittedWords.length==1&&splittedWords[0].equals(""))
 		{
 			splittedWords = new String[0];
+		}
+		
+		if(splittedWords.length > 0)
+		{
+			for(int i = 0; i < splittedWords.length; i++)
+			{
+				int indexOfSpace = splittedWords[i].indexOf(' ');
+				if(indexOfSpace>=0)
+				{
+					splittedWords[i] = splittedWords[i].substring(0, indexOfSpace);
+				}
+			}
 		}
 		
 		ArrayList<String> returnValue = new ArrayList<String>();
@@ -103,7 +121,7 @@ public class WordLinks
 		for(int i = 0 ;i < splittedWords.length; i++)
 		{
 			returnValue.add(splittedWords[i]);
-		}
+		}	
 		
 		return returnValue;
 		
@@ -201,17 +219,16 @@ public class WordLinks
 		ArrayList<String> words;
 		System.out.println("Enter a comma separated list of words (or an empty list to quit):");
 		while((words = readWordList()).size()!=0)
-		{
+		{		
 			if(isWordChain(words))
 			{
 				System.out.println("Valid chain of words from Lewis Carroll's word-links game.");
 			}
 			else
 			{
-				System.out.println("Not a valid chain of words from Lewis Carroll's word-links game");
+				System.out.println("Not a valid chain of words from Lewis Carroll's word-links game.");
 			}
-			System.out.println("Enter a comma separated list of words (or an empty list to quit)");
-		}
-		
+			System.out.println("Enter a comma separated list of words (or an empty list to quit):");
+		}		
 	}
 }
